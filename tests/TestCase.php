b7dc5445605171306f2e3e69955de408e0da9540
Wandes Cardoso <wandes2030@gmail.com>
2024-01-20 00:00:52 -0300

    chore: add orchestra for TDD

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,8 +2,23 @@
 
 namespace Tests;
 
-use PHPUnit\Framework\TestCase as BaseTestCase;
+use Orchestra\Testbench\TestCase as BaseTestCase;
+use WandesCardoso\MercadoPago\MercadoPagoServiceProvider;
+use WandesCardoso\MercadoPago\Facades\MercadoPago;
 
 abstract class TestCase extends BaseTestCase
 {
+    protected function getPackageProviders($app): array
+    {
+        return [
+            MercadoPagoServiceProvider::class,
+        ];
+    }
+
+    protected function getPackageAliases($app): array
+    {
+        return [
+            'MercadoPago' => MercadoPago::class,
+        ];
+    }
 }
