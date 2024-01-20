cf480cb2f4e4bec1c3e10ce66d9833705a6e962b
Wandes Cardoso <wandes2030@gmail.com>
2024-01-20 16:03:32 -0300

    feat: add list of accepted currencies

---
+++ src/Enums/Currency.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace WandesCardoso\MercadoPago\Enums;
+
+enum Currency: string
+{
+    case ARS = 'ARS';
+    case BRL = 'BRL';
+    case CLP = 'CLP';
+    case MXN = 'MXN';
+    case COP = 'COP';
+    case PEN = 'PEN';
+    case UYU = 'UYU';
+
+}
